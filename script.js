7ade876666124bdb7d8fc6c83a9bd0dba8d51919
MailsonHit <hitbdm22@gmail.com>
2026-01-05 01:29:37 -0300

    Add initial script.js with success message

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("HitScore carregado com sucesso 🚀");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const msg = document.createElement("p");
+  msg.innerText = "JavaScript funcionando!";
+  document.body.appendChild(msg);
+});
